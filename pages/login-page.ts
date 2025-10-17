89514156fef91403010427ee6f8ed58dc60f03c3
Vince Palencia <vince.palencia@innovativesolutionph.com>
2025-10-17 12:53:44 +0800

    login flaky test fix

--- pages/login-page.ts
+++ pages/login-page.ts
@@ -24,7 +24,10 @@ export class LoginPage {
     async login(email: string, password: string){
         await this.emailField.fill(email)
         await this.passwordField.fill(password)
-        await this.loginBtn.click()
+        await Promise.all([
+            this.page.waitForNavigation({ url: /locations/ }),
+            this.loginBtn.click()
+        ])
     }
 
     async clickLoginBtn(){
