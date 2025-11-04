b8149a75cf793ddc618fc2218c4134b611c9590d
Vince Palencia <vince.palencia@innovativesolutionph.com>
2025-11-04 06:34:38 -0500

    log assertions for ai

--- webpos-tests/calendar.spec.ts
+++ webpos-tests/calendar.spec.ts
@@ -25,21 +25,25 @@ test.describe('Calendar Tests', () => {
     
     test('[TC-106] Create Appointment', async ({page}) => {
         await createAppointment.clickNewBtn()
+        console.warn('AI assertion')
         const cadOptions = await ai('Get the text of the two buttons in the "New" modal', { page, test })
         console.log('Cad Options: ' +cadOptions)
         await createAppointment.clickAppointmentOption()
         await createAppointment.selectServiceCategory()
         await createAppointment.selectService()
+        console.warn('AI assertion')
         const firstCategory = await ai('Get the text of the second category below the Categories text', {page,test})
         console.log('Second Category: ' +firstCategory)
+        console.warn('AI assertion')
         const firstService = await ai('Get the text of the second service option below the search bar', {page,test})
         console.log('First Service: ' +firstService)
+        console.warn('AI assertion')
         const modalView = await ai('Verify that the page displays "New Appointment", "Categories", and "Search service" texts', { page, test })
         console.log('Is the modal view correct?: ' +modalView)
         await createAppointment.selectCustomer()
         await createAppointment.selectDate()
         await createAppointment.clickFinishBtn()
-        await toastComponent.verifyMessage('Created successfully!!!')
+        await toastComponent.verifyMessage('Created successfully')
     })
 
     test('[TC-130] Create appointment via calendar shortcut', async () => {
